daa7e5827d8fe57c51ac9f6d905eb31db66a6773
oksanakney <oksanakney@gmail.com>
2024-08-12 15:57:25 +0300

    Update 05

---
+++ 01.FirstSteps-Lab/05.GreetingByName/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _05.GreetingByName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine()!;
+            Console.WriteLine("Hello, " + name + "!");
+        }
+    }
+}
