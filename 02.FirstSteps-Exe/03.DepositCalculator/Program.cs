0afc57e980d993fa5e2179abd6aba719bb6858d6
oksanakney <oksanakney@gmail.com>
2024-08-12 19:06:36 +0300

    Update 03

---
+++ 02.FirstSteps-Exe/03.DepositCalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _03.DepositCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            double deposit = double.Parse(Console.ReadLine()!);
+            int term = int.Parse(Console.ReadLine()!);
+            double annualInterestRate = double.Parse(Console.ReadLine()!);
+            
+            double result = deposit + term * ((deposit * annualInterestRate / 100) / 12);
+
+            Console.WriteLine(result);
+        }
+    }
+}
