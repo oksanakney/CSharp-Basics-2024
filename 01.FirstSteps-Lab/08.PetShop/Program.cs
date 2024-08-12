3d26096fdda02b29795e1058553e44b31ad9ee74
oksanakney <oksanakney@gmail.com>
2024-08-12 15:58:31 +0300

    Update 08

---
+++ 01.FirstSteps-Lab/08.PetShop/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _08.PetShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogFoodCount = int.Parse(Console.ReadLine()!);
+            int catFoodCount = int.Parse(Console.ReadLine()!);
+
+            double dogFoodPrice = 2.5;
+            double catFoodPrice = 4;
+
+            double sum = dogFoodCount * dogFoodPrice + catFoodCount * catFoodPrice;
+
+            Console.WriteLine($"{sum} lv.");
+        }
+    }
+}
