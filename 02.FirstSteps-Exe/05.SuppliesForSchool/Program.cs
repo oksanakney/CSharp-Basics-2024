360eca984399b283d1d90bef56c496aed0542cba
oksanakney <oksanakney@gmail.com>
2024-08-12 19:06:50 +0300

    Update 05

---
+++ 02.FirstSteps-Exe/05.SuppliesForSchool/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _05.SuppliesForSchool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pens = int.Parse(Console.ReadLine()!);
+            int markers = int.Parse(Console.ReadLine()!);
+            int detergent = int.Parse(Console.ReadLine()!);
+            double discount = double.Parse(Console.ReadLine()!);
+           
+            double penPrice = 5.8;
+            double markerPrice = 7.2;
+            double detergentPrice = 1.2;
+            double discountRate = 1 - (discount / 100);
+            double sum = (penPrice * pens + markerPrice * markers + detergentPrice * detergent) * discountRate;
+
+            Console.WriteLine(sum);
+        }
+    }
+}
