a4460c953c57835d68f033fe1cc3622532b8a130
oksanakney <oksanakney@gmail.com>
2024-08-12 19:06:24 +0300

    Update 02

---
+++ 02.FirstSteps-Exe/02.RadiantsToDegrees/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _02.RadiantsToDegrees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radiants = double.Parse(Console.ReadLine()!);
+            double degrees = radiants * 180 / Math.PI;
+            Console.WriteLine(degrees);
+        }
+    }
+}
