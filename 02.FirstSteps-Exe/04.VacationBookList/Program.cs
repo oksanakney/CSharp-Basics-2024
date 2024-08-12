648955aa2a91f2d8a2e6188575eb35540f45ec37
oksanakney <oksanakney@gmail.com>
2024-08-12 19:06:43 +0300

    Update 04

---
+++ 02.FirstSteps-Exe/04.VacationBookList/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _04.VacationBookList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {          
+            int pages = int.Parse(Console.ReadLine()!);
+            int pagesPerHour = int.Parse(Console.ReadLine()!);
+            int days = int.Parse(Console.ReadLine()!);
+
+            int pagesPerDay = pages / pagesPerHour / days;
+            Console.WriteLine(pagesPerDay);
+        }
+    }
+}
