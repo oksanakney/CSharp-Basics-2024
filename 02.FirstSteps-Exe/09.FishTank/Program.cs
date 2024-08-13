68cc760c470884e55bcacd847eca427dac6888fd
oksanakney <oksanakney@gmail.com>
2024-08-13 20:50:40 +0300

    Updated 09

---
+++ 02.FirstSteps-Exe/09.FishTank/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _09.FishTank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = int.Parse(Console.ReadLine()!);
+            double width = int.Parse(Console.ReadLine()!);
+            double height = int.Parse(Console.ReadLine()!);
+            double percentage = double.Parse(Console.ReadLine()!);
+
+            double volumeInLiters = (length * width * height) / 1000;
+            double volumeWaterNeeded = volumeInLiters * (1 - percentage / 100);
+
+            Console.WriteLine(volumeWaterNeeded);
+        }
+    }
+}
