8b436f446bbe69b632ae7c2cdae4120de92533bf
oksanakney <oksanakney@gmail.com>
2024-08-13 20:50:32 +0300

    Updated 08

---
+++ 02.FirstSteps-Exe/08.BasketballEquipment/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _08.BasketballEquipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double annualTax = double.Parse(Console.ReadLine()!);
+
+            double boots = annualTax * 0.6;
+            double dress = boots * 0.8;
+            double ball = dress / 4;
+            double accessories = ball / 5;
+
+            double sum = annualTax + boots + dress + ball + accessories;
+
+            Console.WriteLine(sum);
+        }
+    }
+}
