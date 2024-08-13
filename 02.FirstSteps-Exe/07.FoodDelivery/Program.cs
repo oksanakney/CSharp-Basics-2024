508c34e3e66f081d2bed1e9ace5c22e218edb3b3
oksanakney <oksanakney@gmail.com>
2024-08-13 20:50:22 +0300

    Updated 07

---
+++ 02.FirstSteps-Exe/07.FoodDelivery/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _07.FoodDelivery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int chicken = int.Parse(Console.ReadLine()!);
+            int fish = int.Parse(Console.ReadLine()!);
+            int vegetarian = int.Parse(Console.ReadLine()!);
+
+            double chickenPrice = 10.35;
+            double fishPrice = 12.4;
+            double vegetarianPrice = 8.15;
+
+            double foodPrice = chicken * chickenPrice + fish * fishPrice + vegetarian * vegetarianPrice;
+            double dessertPrice = foodPrice * 0.2;
+            double totalPrice = foodPrice + dessertPrice + 2.5;
+
+            Console.WriteLine(totalPrice);
+        }
+    }
+}
